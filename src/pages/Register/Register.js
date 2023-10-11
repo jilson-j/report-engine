8e8a89d78aa0a4dbbf89cce7482dfb69c44c9fe4
jilson-j <jilsonjildas@gmail.com>
2023-10-11 22:42:19 +0530

    login ui implemented

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 import React from 'react'
 import './Register.css'
-import { Button, Typography } from '@mui/material'
+import { Button, TextField, Typography } from '@mui/material'
 const Register = () => {
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
+  const[erroremail,setErrorEmail]=useState("")
+  const[errorpassword,seterrorPassword]=useState("")
   return (
     <div className='container'>
       <div className='flex-container'>
@@ -10,7 +15,11 @@ const Register = () => {
           <h1 className='title'> L&T EduTech Analytics! </h1>
         </div>
         <div className='form-section'>
-          <h1>test</h1>
+          <h3>LOGIN</h3>
+          <TextField label="Email" placeholder='Please enter email' className="input-field" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <TextField label="Password" placeholder='Please enter password' className="input-field"  value={password} onChange={(e)=>setPassword(e.target.password)}/>
+          <Button type="button" variant="contained">Login</Button>
+
         </div>
 
         </div>
